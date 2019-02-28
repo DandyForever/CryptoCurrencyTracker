7e6bb0703eb9b53a4995ea2b033dfe7d4a4f19b5
IgorOzherelev <43535311+IgorOzherelev@users.noreply.github.com>
2019-02-28 22:14:30 +0300

    Add files via upload

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,30 +1,23 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS currencies(
   id BIGINT PRIMARY KEY,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  name VARCHAR(100) UNIQUE NOT NULL,
-  password_hash VARCHAR(255),
-  password_salt VARCHAR(32)
+  curr_val NUMERIC NOT NULL,
+  name VARCHAR(100) NOT NULL
 );
 
-COMMENT ON TABLE users IS 'Table contains the users'' data';
-COMMENT ON COLUMN users.id IS 'User ID';
-COMMENT ON COLUMN users.email IS 'User email';
-COMMENT ON COLUMN users.name IS 'Username';
-COMMENT ON COLUMN users.password_hash IS 'User''s password hash';
-COMMENT ON COLUMN users.password_salt IS 'A salt to calculate a password hash';
-
-CREATE SEQUENCE IF NOT EXIST user_id_sequence START WITH 1 MINVALUE 1 INCREMENT BY 1;
-COMMENT ON SEQUENCE user_id_sequence IS 'Sequence for ID of table ''users''';
 
 
-CREATE TABLE IF NOT EXISTS user_subscriptions (
-  user_id BIGINT REFERENCES users(id),
+
+CREATE TABLE IF NOT EXISTS currencies_days(
   currency_id BIGINT REFERENCES currencies(id),
-  user_val NUMERIC,
-  CONSTRAINT user_currency_pk PRIMARY KEY (user_id, currency_id)
+  curr_date TIMESTAMP,
+  CONSTRAINT currency_day_pk PRIMARY KEY(currency_id, curr_date),
+  aver_day NUMERIC
 );
 
-COMMENT ON TABLE user_subscriptions IS 'Linkage table between tables ''users'' and ''currencies'' ';
-COMMENT ON COLUMN user_subscriptions.user_id IS 'User ID';
-COMMENT ON COLUMN user_subscriptions.currency_id IS 'Currency ID';
-COMMENT ON COLUMN user_subscriptions.user_val IS 'User value of money';
+/*INSERT INTO "currencies" VALUES(1, 100, 'BTC/USD');
+INSERT INTO "currencies_days" VALUES(1, CURRENT_TIMESTAMP, 100);*/
+
+
+
+
+
