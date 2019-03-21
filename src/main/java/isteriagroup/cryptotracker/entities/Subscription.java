d67c89cbbc4f94cac80272afeb4898adfcab3edc
IgorOzherelev <43535311+IgorOzherelev@users.noreply.github.com>
2019-03-21 19:47:52 +0300

    Subscription.java provides SubscriptionPK.java
    added complicated primary key

--- src/main/java/isteriagroup/cryptotracker/entities/Subscription.java
+++ src/main/java/isteriagroup/cryptotracker/entities/Subscription.java
@@ -6,31 +6,23 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+
 
 @Entity
-@Table(name = "USERS_SUBSCRIPTIONS")
+@Table(name = "SUBSCRIPTIONS")
 @ToString(exclude = {"user", "currency"})
 @NoArgsConstructor
-public class Subscription {
+public class Subscription{
 
-    @Id
-    @Column(name = "USER_CURRENCY_PK")
+    @EmbeddedId
     @Getter
     @Setter
     private SubscriptionPK subscriptionPK;
-
 
     @Column(name = "USER_VAL")
     @Getter
     @Setter
     private Float userVal;
-
-    @Column(name = "USER_ID")
-    @NotNull
-    @Getter
-    @Setter
-    private Long userId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID", insertable = false, updatable = false)
@@ -38,22 +30,15 @@ public class Subscription {
     @Setter
     private User user;
 
-    @Column(name = "CURRENCY_ID")
-    @NotNull
-    @Getter
-    @Setter
-    private Long currencyId;
-
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "CURRENCY_ID", insertable = false, updatable = false)
     @Getter
     @Setter
     private Currency currency;
 
-    public Subscription(Float userVal, Long userId, Long currencyId) {
-        this.userVal = userVal;
-        this.userId = userId;
-        this.currencyId = currencyId;
-    }
 
+    public Subscription(SubscriptionPK subscriptionPK, Float userVal) {
+        this.subscriptionPK = subscriptionPK;
+        this.userVal = userVal;
+    }
 }
